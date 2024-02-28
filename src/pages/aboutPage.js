63b2e39c1774e2a741cbca2c3111c9d05866c044
chintan224 <161326876+chintan224@users.noreply.github.com>
2024-02-28 15:17:15 +0530

    commit

---
+++ src/pages/aboutPage.js
@@ -0,0 +1,16 @@
+export default function AboutPage() {
+    return (
+        <div className="jumbotron blue-bg">
+            <h1>Credit Suisse</h1>
+            <hr/>
+            <h1> We Are Learning React</h1>
+
+            <div>
+                <p>React Hooks</p>
+                <p>React Routing</p>
+                <p>Bootstrap</p>
+            </div>
+
+        </div>
+    )
+}
---
+++ src/pages/customerPage.js
@@ -0,0 +1,7 @@
+import CustomerList from "../components/customers/customerList";
+
+export default function CustomerPage() {
+    return (
+        <CustomerList />
+    )
+}

484d8e4b670d49814ea8c74b97a43e9dfdb2a49f
chintan224 <161326876+chintan224@users.noreply.github.com>
2024-02-28 15:10:30 +0530

    updated

---
+++ src/components/customers/CustomerForm.js
@@ -0,0 +1,55 @@
+import {useState} from 'react'
+
+export default function CustomerForm(props) {
+
+    const [formData, setNewFormData] = useState({
+        id: "",
+        firstName: "",
+        lastName: "",
+        email: ""
+    })
+
+    const handleChange = (event) => {
+        const {name, value} = event.target
+        let customerData = {...formData}
+        customerData[name] = value
+        setNewFormData(customerData)
+    }
+
+    const saveCustomer = () => {
+        const customer = {...formData}
+        props.addCustomer(customer)
+    }
+
+    const handleSubmit = (event) => {
+        event.preventDefault()
+        saveCustomer()
+    }
+
+    return (
+        <div>
+            <h3>Add Customer</h3>
+            <hr/>
+            <form className='ui form' onSubmit={(event)=>handleSubmit(event)}>
+                <div className='form-group'>
+                    <input type='text'
+                        name="firstName"
+                        value={formData.firstName}
+                        onChange={(event) =>handleChange(event)}/> 
+                </div>
+                <div className='form-group'>
+                    <input type='text'
+                        name="lastName"
+                        value={formData.lastName}
+                        onChange={(event) =>handleChange(event)}/> 
+                </div>
+                <div className='form-group'>
+                    <input type='text'
+                        name="email"
+                        value={formData.email}
+                        onChange={(event) =>handleChange(event)}/> 
+                </div>
+            </form>
+        </div>
+    )
+}
---
+++ src/components/customers/CustomerList.js
@@ -0,0 +1,62 @@
+import {useState} from 'react'
+import customerdata from './customers.json'
+import CustomerDetails from './CustomerDeatils'
+import CustomerForm from './CustomerForm'
+
+export default function CustomerList () {
+
+    const[customers, setNewCustomer] = useState(customerdata)
+    const[selectedCustomer, setNewSelectedCustomer] = useState(null)
+
+    const onCustomerSelect = (customer) => {
+        setNewSelectedCustomer(customer)
+    }
+
+    const tableRows = customers.map((customer, index) => {
+        return (
+            <tr onClick={(event) => onCustomerSelect(customer)} key={customer.id} >
+                <td>{customer.id}</td>
+                <td>{customer.firstName}</td>
+                <td>{customer.lastName}</td>
+                <td>{customer.email}</td>
+            </tr>
+        )
+    })
+
+    const createCustomer = (newCustomer) => {
+        const totalLength = customers.length
+        newCustomer.id = totalLength+1
+        let newList=[...customers]
+        newList.push(newCustomer)
+        setNewCustomer(newList)
+    }
+
+    return(
+        <div>
+            <table className='table table-bordered table-hover table-stripped'>
+                <thead>
+                    <tr>
+                        <th>Id</th>
+                        <th>First Name</th>
+                        <th>Last Name</th>
+                        <th>Email</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {tableRows}
+                </tbody>
+            </table>
+            <br/>
+            <br/>
+            <div className='row'>
+                <div className='col-md-6'>
+                    <CustomerForm addCustomer={createCustomer}/>
+                </div>
+                <div className='col-md-4'>
+                    {selectedCustomer ? <CustomerDetails customer={selectedCustomer} /> : null}
+                </div>
+            </div>                
+        </div>
+    )
+
+}
